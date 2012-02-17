0216054d1d7034bfc4951c1d6037eec588686ae5
Graham McMillan <mcmgraham@gmail.com>
2012-02-16 21:19:11 -0500

    first tests :)

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,2 +1,20 @@
 class Bike
+  
+  attr_accessor :bikes
+  
+  def initialize
+    @bikes = [{:bike_id   => 1,
+              :owner_id  => 1,
+              :name      => 'Bianchi', 
+              :parts     => ['frame', 'tires', 'saddle']},
+             {:bike_id   => 2,
+              :owner_id  => 2,
+              :name      => 'Trek', 
+              :parts     => ['frame', 'tires', 'saddle']}]
+  end
+
+  def list
+    bikes
+  end
+  
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,29 @@
 require 'spec_helper'
 
 describe Bike do
+  
+  before :each do
+    @bike = Bike.new
+  end
+  
+  describe "#new" do
+    it "should return a Bike object" do
+      @bike.should be_an_instance_of Bike
+    end
+  end
+  
+  describe "#list" do
+    it "should return an array" do
+      bikes = @bike.list
+      bikes.should be_an(Array)
+    end
+    
+    it "should return a hash for each bike" do
+      bikes = @bike.list
+      bikes.each do |b|
+        b.should be_a(Hash)
+      end
+    end
+
+  end
 end
