0eec9d54c853f823de6c700efdfb277208208bf8
Graham McMillan <mcmgraham@gmail.com>
2012-02-16 21:41:11 -0500

    dry

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -13,17 +13,20 @@ describe Bike do
   end
   
   describe "#list" do
+    
+    before :each do
+      @bikes = @bike.list
+    end
+    
     it "should return an array" do
-      bikes = @bike.list
-      bikes.should be_an(Array)
+      @bikes.should be_an(Array)
     end
     
     it "should return a hash for each bike" do
-      bikes = @bike.list
-      bikes.each do |b|
+      @bikes.each do |b|
         b.should be_a(Hash)
       end
     end
-
+    
   end
 end
